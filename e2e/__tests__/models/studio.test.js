afdb02ce21ca0b3fd827af19588fb1cef55b69c0
JOHN NELSON <JohnNelson4850@gmail.com>
2019-09-26 15:38:49 -0700

    studio model test confirmation

--- e2e/__tests__/models/studio.test.js
+++ e2e/__tests__/models/studio.test.js
@@ -1,2 +1,29 @@
+// eslint-disable-next-line no-unused-vars
 const mongoose = require('mongoose');
 const Studio = require('../../../lib/models/studio');
+
+describe('Studio Model', ()=> {
+  it('validates a studio model', () => {
+    const data = {
+      name: 'Marvel',
+      address: { 
+        city: 'Burbank',
+        state: 'California',
+        country: 'USA'
+      }
+    };
+    const studio = new Studio(data);
+    const errors = studio.validateSync();
+    expect(errors).toBeUndefined();
+
+    const json = studio.toJSON();
+    expect(json).toEqual({
+      ...data,
+      _id: expect.any(Object),
+    });
+  });
+
+
+
+
+});
