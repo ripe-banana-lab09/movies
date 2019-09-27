a34081bcf80055b4e4c302ab20f7362f6419e4d6
JOHN NELSON <JohnNelson4850@gmail.com>
2019-09-27 16:49:04 -0700

    film test, still has issues

--- e2e/__tests__/routes/films.test.js
+++ e2e/__tests__/routes/films.test.js
@@ -13,10 +13,12 @@ describe('Films Route Test', () => {
     title: 'Spider-man',
     studio: [],
     released: 2017,
-    cast: [{
-      role: 'Spider-man',
-      actor: []
-    }]
+    cast: [
+      {
+        role: 'Spider-man',
+        actor: []
+      }
+    ]
   };
 
   const actor = {
@@ -27,7 +29,7 @@ describe('Films Route Test', () => {
 
   const studio = {
     name: 'Marvel',
-    address: { 
+    address: {
       city: 'Burbank',
       state: 'California',
       country: 'USA'
@@ -35,18 +37,40 @@ describe('Films Route Test', () => {
   };
 
   function postFilm(film) {
+    return Promise.all([]);
     return request
       .post('/api/actors')
-      .send(actor, studio)
+      .send(actor)
       .expect(200)
       .then(({ body }) => {
         film.actor[0] = body._id;
+        return request
+          .post('/api/films')
+          .send(film)
+          .expect(200);
+      })
+      .then(({ body }) => body)
+      .post('/api/studios')
+      .send(studio)
+      .expect(200)
+      .then(({ body }) => {
         film.studio[0] = body._id;
         return request
           .post('/api/films')
           .send(film)
-          .expect(200)
+          .expect(200);
       })
       .then(({ body }) => body);
   }
+  it('posts a film', () => {
+    return postFilm(film).then(film => {
+      expect(film).toMatchInlineSnapshot(
+        {
+          //   _id: expect.any(String),
+          //   studio:
+        },
+        `Object {}`
+      );
+    });
+  });
 });
